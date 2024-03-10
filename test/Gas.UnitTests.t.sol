d4e6755f3c0ea6b59365f446d11ba35310633139
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-03-11 02:51:02 +0800

    Gas Optimization

--- test/Gas.UnitTests.t.sol
+++ test/Gas.UnitTests.t.sol
@@ -57,4 +57,140 @@ contract GasTest is Test {
         gas.addToWhitelist(_userAddrs, _tier);
         vm.stopPrank();
     }
+
+    //----------------------------------------------------//
+    //------------- Test whitelist Transfers -------------//
+    //----------------------------------------------------//
+
+    function test_whitelistTransfer(
+        address _recipient,
+        address _sender,
+        uint256 _amount,
+        string calldata _name,
+        uint256 _tier
+    ) public {
+        _amount = bound(_amount, 0, gas.balanceOf(owner));
+        vm.assume(_amount > 3);
+        vm.assume(bytes(_name).length < 9);
+        _tier = bound(_tier, 1, 244);
+        vm.startPrank(owner);
+        gas.transfer(_sender, _amount, _name);
+        gas.addToWhitelist(_sender, _tier);
+        vm.stopPrank();
+        vm.prank(_sender);
+        gas.whiteTransfer(_recipient, _amount);
+        (bool a, uint256 b) = gas.getPaymentStatus(address(_sender));
+        console.log(a);
+        assertEq(a, true);
+        assertEq(b, _amount);
+    }
+
+    // Reverts if tiers out of bounds
+    function test_tiersReverts(address _userAddrs, uint256 _tier) public {
+        vm.assume(_userAddrs != address(gas));
+        vm.assume(_tier > 254);
+        vm.prank(owner);
+        vm.expectRevert();
+        gas.addToWhitelist(_userAddrs, _tier);
+    }
+
+    // Expect Event -->
+    event WhiteListTransfer(address indexed);
+
+    function test_whitelistEvents(
+        address _recipient,
+        address _sender,
+        uint256 _amount,
+        string calldata _name,
+        uint256 _tier
+    ) public {
+        _amount = bound(_amount, 0, gas.balanceOf(owner));
+        vm.assume(_amount > 3);
+        vm.assume(bytes(_name).length < 9);
+        _tier = bound(_tier, 1, 244);
+        vm.startPrank(owner);
+        gas.transfer(_sender, _amount, _name);
+        gas.addToWhitelist(_sender, _tier);
+        vm.stopPrank();
+        vm.startPrank(_sender);
+        vm.expectEmit(true, false, false, true);
+        emit WhiteListTransfer(_recipient);
+        gas.whiteTransfer(_recipient, _amount);
+        vm.stopPrank();
+    }
+
+    /* whiteTranfer balance logic. 
+        balances[senderOfTx] -= _amount;
+        balances[_recipient] += _amount;
+        balances[senderOfTx] += whitelist[senderOfTx];
+        balances[_recipient] -= whitelist[senderOfTx]; 
+        */
+
+    //    check balances update
+    function test_WhiteTransferAmountUpdate(
+        address _recipient,
+        address _sender,
+        uint256 _amount,
+        string calldata _name,
+        uint256 _tier
+    ) public {
+        uint256 _preRecipientAmount = gas.balances(_recipient) + 0;
+        vm.assume(_recipient != address(0));
+        vm.assume(_sender != address(0));
+        _amount = bound(_amount, 0, gas.balanceOf(owner));
+        _tier = bound(_tier, 1, 244);
+        vm.assume(_amount > 3);
+        vm.assume(bytes(_name).length < 9 && bytes(_name).length > 0);
+        vm.startPrank(owner);
+        gas.transfer(_sender, _amount, _name);
+        uint256 _preSenderAmount = gas.balances(_sender);
+        gas.addToWhitelist(_sender, _tier);
+        vm.stopPrank();
+        vm.prank(_sender);
+        gas.whiteTransfer(_recipient, _amount);
+        assertEq(gas.balances(_sender), (_preSenderAmount - _amount) + gas.whitelist(_sender));
+        assertEq(gas.balances(_recipient), (_preRecipientAmount + _amount) - gas.whitelist(_sender));
+    }
+
+    function test_BalanceOf() public {
+        uint256 bal = gas.balanceOf(owner);
+        assertEq(bal, totalSupply);
+    }
+
+    function test_CheckForAdmin() public {
+        bool isAdmin = gas.checkForAdmin(owner);
+        assertEq(isAdmin, true);
+    }
+
+    // TODO: No Specification
+    function testGetPaymentHistory() public {}
+
+    // TODO: No Specification
+    function testGetTradingMode() public {}
+
+    // TODO: No Specification
+    function testAddHistory() public {}
+
+    function test_Transfer(uint256 _amount, address _recipient) public {
+        vm.assume(_amount <= totalSupply);
+        vm.startPrank(owner);
+
+        uint256 ownerBal = gas.balanceOf(owner);
+        uint256 balBefore = gas.balanceOf(_recipient);
+        gas.transfer(_recipient, _amount, "name");
+        uint256 balAfter = gas.balanceOf(_recipient);
+
+        assertEq(balAfter, balBefore + _amount);
+        assertEq(gas.balanceOf(owner), ownerBal - _amount);
+    }
+
+    function test_AddToWhitelist(address user, uint256 _tier) public {
+        vm.expectRevert();
+        vm.startPrank(user);
+        vm.assume(user != owner);
+        gas.addToWhitelist(user, _tier);
+        vm.stopPrank();
+    }
+
+    function testGetPaymentStatus(address sender) public {}
 }
