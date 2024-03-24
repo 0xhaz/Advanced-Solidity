a73386404dd71cb71582c95d871c51bb94489750
0xhaz <28639279+0xhaz@users.noreply.github.com>
2024-03-25 05:32:49 +0800

    Ownable updated

--- src/Homework-8/Ownable.sol
+++ src/Homework-8/Ownable.sol
@@ -68,7 +68,7 @@ abstract contract Ownable is Context {
      * `onlyOwner` functions anymore. Can only be called by the current owner
      *
      * NOTE: Renouncing ownership will leave the contract without an owner,
-     * thereby removing any functionality that is only available to the owner.
+     * thereby removing any functionality that is only available to the owner
      */
     function renounceOwnership() public virtual onlyOwner {
         _transferOwnership(address(0));
